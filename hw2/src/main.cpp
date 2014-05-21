4040f0ca55559f8465d17ff50d66c05fa5cde65f
Matthew Grasinger <grasingerm@gmail.com>
2014-05-21 16:09:12 -0400

    started hw2 main

--- hw2/src/main.cpp
+++ hw2/src/main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
+#include <armadillo>
+#include <array>
+#include "element.h"
 
 using namespace std;
 
+/**
+ * Essential boundary condition
+ */
+class EBC
+{
+public:
+    EBC() {}
+    EBC(const unsigned int g_dof, const double val) : g_dof(g_dof), val(val) {}
+    unsigned int g_dof;
+    double val;
+};
+
 int main(int argc, char* argv[])
 {
-    double (*foo)(const double);
-
-    foo = &yolo;
-
-    cout << foo(3.4) << endl;
-
-    return 0;
+    const array<double, 2> x_domain { {0.0, 1.0} };
+    array<EBC, 2> u_ebcs { {EBC(), EBC()} };
 }
 

9be6c5ec948d95eeb986240e30085f75828d08ab
Matthew Grasinger <grasingerm@gmail.com>
2015-01-05 18:12:49 -0500

    gradient
    Add: gradient of shape functions with respect to spatial or ref coords

--- hw3/oct/fem.jl
+++ hw3/oct/fem.jl
@@ -37,12 +37,26 @@ macro dN4deta_Q4(xi::Real)
   return (-(xi)/4.);
 end
 
+#! \macro Check parent coordinates to make sure they fall within parent element
+#!
+#! \param xi Parent coordinate in x-direction
+#! \param eta Parent coordinate in y-direction
+#! \return Assertions to check parent coordinates for correctness
+macro check_par_coords_2D(xi::Real, eta::Real)
+  return quote
+    @assert(xi >= -1.0 && xi <= 1.0, "-1.0 <= xi <= 1.0 must be true");
+    @assert(eta >= -1.0 && eta <= 1.0, "-1.0 <= eta <= 1.0 must be true");
+  end
+end
+
 #! N matrix for a Q4 element
 #!
 #! \param xi Parent coordinate in x-direction
 #! \param eta Parent coordinate in y-direction
 #! \return N matrix
 function N_Q4(xi::Real, eta::Real)
+  @check_par_coords_2D(xi, eta);
+
   Float64[
     @N1_Q4(xi, eta) 0 @N2_Q4(xi, eta) 0 @N3_Q4(xi, eta) 0 @N4_Q4(xi, eta) 0;
     0 @N1_Q4(xi, eta) 0 @N2_Q4(xi, eta) 0 @N3_Q4(xi, eta) 0 @N4_Q4(xi, eta);
@@ -58,41 +72,71 @@ function ref_glob_to_par(N::Array{Float64,2}, X::Array{Float64,2})
   return N*X;
 end
 
-#! Deformation gradient to map parent spatial coords to deformed spatial coords
+#! Derivative of shape functions with respect to parent coords, Q4
 #!
 #! \param xi Parent coordinate in x-direction
 #! \param eta Parent coordinate in y-direction
-#! \param x Two dimensional matrix of deformed, spatial, nodal coordinates
-#! \return Deformation gradient
-function defgrad_par_to_deformed(xi::Real, eta::Real, x::Array{Float64,2})
-  dN_dxi = Float64[
+#! \return D X N matrix, dN_j / dx_i
+macro dN_dxi_Q4(xi::Real, eta::Real)
+  return (Float64[
     @dN1_dxi(eta) @dN2_dxi(eta) @dN3_dxi(eta) @dN4_dxi(eta);
     @dN1_deta(xi) @dN2_deta(xi) @dN3_deta(xi) @dN4_deta(xi);
-    ];
-
-  F_t = dN_dxi * x;
-  return F_t';
+    ]);
 end
 
-#! @macro Jacobian determinant that relates diff areas from parrent to current
+#! Transpose of deformation gradient to map parent spatial coords to deformed 
+#! spatial coords
 #!
-#! \param F_xi Deformation gradient mapping parent to spatial
+#! \param xi Parent coordinate in x-direction
+#! \param eta Parent coordinate in y-direction
+#! \param x Matrix of deformed, spatial, nodal coordinates in table notation
+#! \return Deformation gradient
+function defgrad_par_to_deformed_t(xi::Real, eta::Real, x::Array{Float64,2})
+  @check_par_coords_2D(xi, eta);
+
+  return @dN_dxi_Q4(xi, eta) * x;
+end
+
+#! \macro Jacobian determinant that relates diff areas from parrent to current
+#!
+#! \param F_xi_t Transpose deformation gradient mapping parent to spatial
 #! \param N Unit vector normal to referential area
 #! \return @expr Jacobian determinant for mapping area
-macro jac_area(F_xi::Array{Float64,2}, N::Array{Float64,1})
-  return (det(F_xi) * norm(inv(F_xi)'*N));
+macro jac_area(F_xi_t::Array{Float64,2}, N::Array{Float64,1})
+  return (det(F_xi_t) * norm(F_xi_t \ N)); #!< A \ B is a polyalgorithm solver
 end
 
 #! Jacobian determinant that relates differential areas from parent to current
 #!
 #! \param xi Parent coordinate in x-direction
 #! \param eta Parent coordinate in y-direction
-#! \param X Two dimensional matrix of referential global coordinates
+#! \param X Matrix of referential global coordinates in table notation
 #! \param N Unit vector normal to the surface of the parent element
 #! \return Jacobian determinant for mapping area
 function jac_area_par_to_deformed(xi::Real, eta::Real, X::Array{Float64,2},
   N::Array{Float64,1})
 
+  @check_par_coords_2D(xi, eta);
+
   F_xi = defgrad_par_to_deformed(xi, eta, X);
   return @jac_area(F_xi, N);
+end
+
+#!!! there are two ways to calculate deformation gradient ...
+# F = F_xi * F_o_xi^-1 !< probably more computationally expensive
+# grad(u) + eye(2), or outer_product(partial{N}{X}, u) + eye(2)
+
+#! Gradient of shape functions with respect to [spatial or referential] coords
+#!
+#! \param F_xi_t Transpose deformation gradient mapping parent to spatial
+#! \param xi Parent coordinate in x-direction
+#! \param eta Parent coordinate in y-direction
+#! \param x Matrix of global coordinates in table notation
+#! \return Gradient of shape functions
+function grad_Q4(F_xi_t::Array{Float64,2}, xi::Real, eta::Real, 
+  x::Array{Float64,2})
+
+  @check_par_coords_2D(xi, eta);
+
+  return inv(F_xi_t) * @dN_dxi_Q4(xi, eta); #!< TODO: fix this!
 end
